bfa26434907655d3cd1785f2d6f518335200d3e8
alicavusoglu <alicavusoglu07@hotmail.com>
2020-02-03 05:45:40 +0300

    local ip

--- routes/relay_local_ip.js
+++ routes/relay_local_ip.js
@@ -2,9 +2,9 @@ import relay_local_ip from '../models/relay_local_ip';
 let router = require('express').Router(); 
 router.route('/')
     .get((req, res) => {
-        relay_local_ip.find({}, (err, relay_local_ips) => {
+        var item=relay_local_ip.findOne().sort({timeStamp: -1}).exec(function (err, input) {
             res.json(relay_local_ips);
-        });
+            });
     })
     .post((req, res) => {
         let input = new relay_local_ip(req.body);
