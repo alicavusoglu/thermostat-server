4f64b7c470ffbdf166dee7f9fa0e8fa7beb5a6c1
alicavusoglu <alicavusoglu07@hotmail.com>
2020-02-09 15:26:08 +0300

    is working

--- models/room_state.js
+++ models/room_state.js
@@ -4,5 +4,6 @@ const Schema = mongoose.Schema;
 const RoomStateModel = new Schema({
     value: {type:Decimal128 },
     timeStamp: { type: Date, default: Date.now },
+    is_working:{type:Boolean}
 });
 export default mongoose.model('room_state', RoomStateModel);
--- models/target_degree.js
+++ models/target_degree.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const TargetDegree = new Schema({
     user:{type:String},
     timeStamp: { type: Date, default: Date.now },
-    value: { type: Decimal128 }
+    value: { type: Decimal128 },
+    is_working:{type:Boolean}
 });
 export default mongoose.model('target_degree', TargetDegree);
